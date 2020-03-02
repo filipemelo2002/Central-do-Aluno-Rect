6ad96559b46139c0e62496767965eeb98eacdf50
mrnobody2002 <filipemelo032@gmail.com>
2020-03-02 11:26:30 -0300

    status bar changes

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from 'react';
-
+import {StatusBar} from 'react-native'
 import Route from './src/routes'
 export default function App() {
-  return <Route/>
+  return (
+    <>
+      <StatusBar backgroundColor="#149dff" barStyle="light-content" />
+      <Route/>
+    </> 
+  )
 }
