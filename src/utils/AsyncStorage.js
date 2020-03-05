07469811056827b62369d285632e4ec75008933e
mrnobody2002 <filipemelo032@gmail.com>
2020-03-05 15:32:45 -0300

    Async storage handler

---
+++ src/utils/AsyncStorage.js
@@ -0,0 +1,26 @@
+import {AsyncStorage} from 'react-native'
+
+
+export async function storeData(key, value){
+  try{
+    await AsyncStorage.setItem(key, value)
+    return true
+  }catch(err){
+    console.log(err)
+    return false
+  }
+}
+
+export async function getData(key){
+  try {
+    const value = await AsyncStorage.getItem(key)
+    if(value !== null) {
+      return JSON.parse(value)
+    }
+    return false
+  } catch(e) {
+    console.log(e)
+    return false
+  }
+}
+
