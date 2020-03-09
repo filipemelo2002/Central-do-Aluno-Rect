afd8c039f9d8a95607d117d93e313b455cc70c70
mrnobody2002 <filipemelo032@gmail.com>
2020-03-09 10:17:52 -0300

    Boletim picker working

--- src/pages/Notas.js
+++ src/pages/Notas.js
@@ -1,10 +1,64 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  Picker,
+  StyleSheet,
+  View,
+  Dimensions
+} from "react-native";
 import Toolbar from "../utils/Toolbar";
+
+import Icon from "react-native-vector-icons/AntDesign";
+
+import { getData } from "../utils/AsyncStorage";
+import api from "../utils/api";
 export default function Notas() {
+  const [boletins, setBoletins] = useState([]);
+
+  useEffect(() => {
+    async function getBoletins() {
+      const { userToken } = await getData("user");
+      const response = await api.get("/boletins", {
+        headers: {
+          userToken
+        }
+      });
+      console.log(response.data);
+      setBoletins(response.data);
+    }
+    getBoletins();
+  }, []);
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, alignItems: "center" }}>
       <Toolbar title="Notas" />
+      <View style={styles.pickerRoot}>
+        <Icon name="book" size={25} color="#149dff" />
+        <Picker style={styles.picker} mode="dialog">
+          {boletins.map((boletin, index) => (
+            <Picker.Item
+              label={boletin.label}
+              value={boletin.ano}
+              key={index}
+            />
+          ))}
+        </Picker>
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  pickerRoot: {
+    width: Dimensions.get("screen").width,
+    height: 60,
+    marginTop: 90,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    padding: 15
+  },
+  picker: {
+    flex: 1
+  }
+});
