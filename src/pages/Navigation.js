020a04095bdd682dfcf064678fba0d1dca5e9ad0
mrnobody2002 <filipemelo032@gmail.com>
2020-03-08 23:29:27 -0300

    navigator set

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -1,19 +1,60 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-// import { Container } from './styles';
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 
-import MyTab from "./utils/MyTab";
+const Tab = createMaterialBottomTabNavigator();
+
+import Icon from "react-native-vector-icons/AntDesign";
+
 import Notas from "./Notas";
 export default function Navigation() {
-  const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
-      <Tab.Navigator tabBar={props => <MyTab {...props} />}>
-        <Tab.Screen component={Notas} name="Notas" />
-        <Tab.Screen component={Notas} name="Frequência" />
-        <Tab.Screen component={Notas} name="Horário" />
-        <Tab.Screen component={Notas} name="Estatísticas" />
+      <Tab.Navigator
+        barStyle={{ backgroundColor: "#fff" }}
+        activeColor="#008df1"
+        inactiveColor="#7f7f7f"
+      >
+        <Tab.Screen
+          component={Notas}
+          name="Notas"
+          options={{
+            tabBarLabel: "Notas",
+            tabBarIcon: ({ color }) => (
+              <Icon color={color} name="table" size={23} />
+            )
+          }}
+        />
+        <Tab.Screen
+          component={Notas}
+          name="Frequência"
+          options={{
+            tabBarLabel: "Frequência",
+            tabBarIcon: ({ color }) => (
+              <Icon color={color} name="user" size={26} />
+            )
+          }}
+        />
+        <Tab.Screen
+          component={Notas}
+          name="Horário"
+          options={{
+            tabBarLabel: "Horário",
+            tabBarIcon: ({ color }) => (
+              <Icon color={color} name="calendar" size={26} />
+            )
+          }}
+        />
+        <Tab.Screen
+          component={Notas}
+          name="Estatísticas"
+          options={{
+            tabBarLabel: "Estatísticas",
+            tabBarIcon: ({ color }) => (
+              <Icon color={color} name="areachart" size={26} />
+            )
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
