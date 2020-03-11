276f972a83093b7f3e678c2838af6e5b7b376a66
mrnobody2002 <filipemelo032@gmail.com>
2020-03-11 13:00:09 -0300

    Internal Storage Handler

--- src/utils/AsyncStorage.js
+++ src/utils/AsyncStorage.js
@@ -1,24 +1,48 @@
 import { AsyncStorage } from "react-native";
 
-export async function storeData(key, value) {
-  try {
-    await AsyncStorage.setItem(key, value);
-    return true;
-  } catch (err) {
-    console.log(err);
-    return false;
+class HandleStorage {
+  async setNotas(boletimId, notas) {
+    const id = `notas-${boletimId}`;
+    return await this.storeData(id, JSON.stringify(notas));
   }
-}
+  async getNotas(boletimId) {
+    const id = `notas-${boletimId}`;
+    const notas = await this.getData(id);
+    return notas ? notas : {};
+  }
+  async setBoletins(boletins) {
+    return await this.storeData("boletins", JSON.stringify(boletins));
+  }
+  async getBoletins() {
+    return await this.getData("boletins");
+  }
+  async setUser(user) {
+    return await this.storeData("user", JSON.stringify(user));
+  }
+  async getUser() {
+    return await this.getData("user");
+  }
 
-export async function getData(key) {
-  try {
-    const value = await AsyncStorage.getItem(key);
-    if (value !== null) {
-      return JSON.parse(value);
+  async storeData(key, value) {
+    try {
+      await AsyncStorage.setItem(key, value);
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
     }
-    return false;
-  } catch (e) {
-    console.log(e);
-    return false;
+  }
+
+  async getData(key) {
+    try {
+      const value = await AsyncStorage.getItem(key);
+      if (value !== null) {
+        return JSON.parse(value);
+      }
+      return false;
+    } catch (e) {
+      return false;
+    }
   }
 }
+export default HandleStorage;
