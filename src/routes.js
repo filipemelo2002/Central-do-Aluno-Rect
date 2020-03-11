c8b8b644c0649af49f3d758148150c679a9d9730
mrnobody2002 <filipemelo032@gmail.com>
2020-03-11 10:58:17 -0300

    Routing correctly

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,41 @@
-import { createSwitchNavigator, createAppContainer } from "react-navigation";
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 import Login from "./pages/Login";
 import Main from "./pages/Main";
-const Routes = createAppContainer(
-  createSwitchNavigator(
-    {
-      Login,
-      Main
-    },
-    { unmountInactiveRoutes: true }
-  )
+const Routes = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{
+          headerShown: false
+        }}
+      />
+      <Stack.Screen
+        name="Main"
+        component={Main}
+        options={{
+          title: "Central do Aluno",
+          headerStyle: {
+            backgroundColor: "#149dff",
+            height: 90
+          },
+          headerLeft: null,
+          headerTitleAlign: "center",
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontSize: 25
+          }
+        }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
 );
 export default Routes;
