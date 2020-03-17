d493052920af10c88d025470ddd726c0b21e0408
mrnobody2002 <filipemelo032@gmail.com>
2020-03-17 11:59:58 -0300

    API Handler Class

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,65 @@
-import axios from 'axios';
+import axios from "axios";
 
+import HandleStorage from "../utils/AsyncStorage";
 const api = axios.create({
-  baseURL: 'https://centraldoaluno.herokuapp.com',
+  baseURL: "https://centraldoaluno.herokuapp.com"
 });
+class ApiHandler {
+  constructor(userToken) {
+    this.api = axios.create({
+      baseURL: "https://centraldoaluno.herokuapp.com"
+    });
+    this.userToken = userToken;
+    this.Storage = new HandleStorage();
+  }
 
-export default api;
+  async getBoletins() {
+    try {
+      const response = await api.get("/boletins", {
+        headers: {
+          userToken: this.userToken
+        }
+      });
+      await this.Storage.setBoletins(response.data);
+      return response.data;
+    } catch (err) {
+      return await this.Storage.getBoletins();
+    }
+  }
+
+  async getNotas(boletimId, ano) {
+    let data = {};
+    try {
+      const response = await api.get(
+        `/boletins/view?boletimId=${boletimId}&ano=${ano}`,
+        {
+          headers: {
+            userToken: this.userToken
+          }
+        }
+      );
+      await this.Storage.setNotas(boletimId, data);
+      return response.data;
+    } catch (err) {
+      return await this.Storage.getNotas(boletimId);
+    }
+  }
+
+  async getFrequencia(boletimId, ano) {
+    try {
+      const response = await api.get(
+        `/faltas?boletimId=${boletimId}&ano=${ano}`,
+        {
+          headers: {
+            userToken: this.userToken
+          }
+        }
+      );
+      await this.Storage.setFrequencia(boletimId, response.data);
+      return response.data;
+    } catch (err) {
+      return await this.Storage.getFrequencia(boletimId);
+    }
+  }
+}
+export default ApiHandler;
