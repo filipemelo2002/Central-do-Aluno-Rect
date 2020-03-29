81d4fd8a2269ccab6e18ea112b137647b99395a9
mrnobody2002 <filipemelo032@gmail.com>
2020-03-28 21:03:23 -0300

    Loading Indicator

--- src/pages/Frequencia.js
+++ src/pages/Frequencia.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { SafeAreaView, ScrollView, StyleSheet, View, Text } from "react-native";
 
 import MyPicker from "./components/MyPicker";
 import HandleStorage from "../utils/AsyncStorage";
 import ApiHandler from "../utils/api";
 
+import Lottie from "lottie-react-native";
 import MyChart, { sanitizePercent, sanitizeAmount } from "./components/MyChart";
 export default function Frequencia() {
   const [boletins, setBoletins] = useState([]);
@@ -13,14 +14,23 @@ export default function Frequencia() {
   const [frequencia, setFrequencia] = useState(null);
   const [percents, setPercents] = useState([0, 0, 0, 0]);
   const [sumFaltas, setSumFaltas] = useState([0, 0, 0, 0, 0]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  const displayGraphs = useMemo(() => (isLoading ? "none" : "flex"), [
+    isLoading
+  ]);
+  const displayLoading = useMemo(() => (isLoading ? "flex" : "none"), [
+    isLoading
+  ]);
   const Storage = new HandleStorage();
   useEffect(() => {
     async function loadBoletins() {
+      setIsLoading(true);
       const { userToken } = await Storage.getUser();
       const data = await Storage.getBoletins();
       setBoletins(data);
       setUser(userToken);
+      setIsLoading(false);
     }
     loadBoletins();
   }, []);
@@ -28,9 +38,11 @@ export default function Frequencia() {
     async function getFrequencia() {
       const { boletimId, ano } = selectedBoletin;
       if (boletimId && ano) {
+        setIsLoading(true);
         const api = new ApiHandler(user);
         const response = await api.getFrequencia(boletimId, ano);
         setFrequencia(response);
+        setIsLoading(false);
       }
     }
     getFrequencia();
@@ -52,16 +64,30 @@ export default function Frequencia() {
         changedState={setSelectedBoletin}
         stateValue={selectedBoletin}
       />
-      <ScrollView>
+      <ScrollView style={{ flex: 1 }}>
         <View style={styles.container}>
-          <MyChart
-            title="Porcentagem de Faltas por Bimestre"
-            values={percents}
-            ySuffix="%"
-          />
-          <MyChart
-            title="Quantidade de Faltas por Bimestre"
-            values={sumFaltas}
+          <View style={{ display: displayGraphs }}>
+            <MyChart
+              title="Porcentagem de Faltas por Bimestre"
+              values={percents}
+              ySuffix="%"
+            />
+            <MyChart
+              title="Quantidade de Faltas por Bimestre"
+              values={sumFaltas}
+            />
+          </View>
+          <Lottie
+            style={{
+              maxWidth: 300,
+              alignSelf: "center",
+              display: displayLoading
+            }}
+            resizeMode="contain"
+            autoSize
+            source={require("../utils/loading.json")}
+            autoPlay
+            loop
           />
         </View>
       </ScrollView>
@@ -75,6 +101,7 @@ const styles = StyleSheet.create({
     padding: 5,
     alignItems: "center",
     paddingTop: 10,
+    justifyContent: "center",
     flexDirection: "column"
   }
 });
